1c37f43a946bdc9c49eb1c3a0d559cf5edfa22c1
Nursultan Akhmetzhanov <nursultan.akhmetzhanov@codemate.com>
2022-05-08 01:58:18 +0300

    Added a link to index page on header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,18 @@
 import { Group, Header, Title } from '@mantine/core';
+import Link from 'next/link';
 import React from 'react';
 
 const CustomHeader = () => {
   return (
     <Header height={60} p='md' className='glassy-background'>
       <Group position='apart'>
-        <Title order={2} style={{ color: 'white' }}>
-          Spotifire!
-        </Title>
+        <Link href={'/'} passHref >
+          <a>
+            <Title order={2} style={{ color: 'white' }}>
+              Spotifire!
+            </Title>
+          </a>
+        </Link>
       </Group>
     </Header>
   );
