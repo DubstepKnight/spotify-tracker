7ff83bcda3a94cfbef403299d13984e1f6822823
Nursultan Akhmetzhanov <nursultan.akhmetzhanov@codemate.com>
2022-11-25 17:12:54 +0200

    chore: added a redirect page when the token is not found

---
+++ pages/please_login.tsx
@@ -0,0 +1,26 @@
+import { Text } from "@mantine/core";
+import { GetServerSideProps, NextPage } from "next";
+import React from "react";
+
+const PleaseLogin: NextPage = () => {
+  return <Text color={"white"}>Please login for the love of God</Text>;
+};
+
+export default PleaseLogin;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  try {
+    return {
+      props: {
+        code: "nothing",
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      props: {
+        error: error,
+      },
+    };
+  }
+};
